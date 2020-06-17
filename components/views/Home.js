0ef3d89885595efc6b894517cdd2b0d999567401
Zukilprime <zukil@juno.com>
2020-06-17 14:39:32 -0500

    Update Home Page

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,10 +1,20 @@
 export default () =>
   `<div>
-  <section>
-  <div>
+  <section id="homePage">
+  <div id="splashPic"
+  style="background-image: url(https://raw.githubusercontent.com/Zukilprime/Savvy-Capstone/master/Pictures/karthik-balakrishnan-NrS53eUKgiE-unsplash_reduced.png);">
   </div>
+    <h3>
+      Welcome to Brave Moose Game Store!
+    </h3>
     <p>
-      Filler content
+      Here at Brave Moose, we pride ourselves in bringing gaming to the masses. Whether it's a quick pick up game with friends, or an epic, world hopping adventure that spans months or years, we are here to help.
+    </p>
+    <p>
+      We stock all the games you could want, and the ones we might not carry, we can order them for you! Need dice, character sheets, or a gaming mat? We have a large variety in stock for all your favorite games.
+    </p>
+    <p>
+      We also strive to be an inclusive gaming store. If you have the time, we have the tools to connect you to other players in the area. It's simple to sign up to our database and start connecting with other local gamers like yourself.
     </p>
   </section>
 </div>`;
