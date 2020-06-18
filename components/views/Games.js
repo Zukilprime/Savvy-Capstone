ed3db1bc1764955e886e4b75ba2fc69de1f7c2a7
Zukilprime <zukil@juno.com>
2020-06-17 20:30:53 -0500

    Resized Photo

--- components/views/Games.js
+++ components/views/Games.js
@@ -10,7 +10,34 @@ Dungeons and Dragons
 </h4>
 <img src="https://raw.githubusercontent.com/Zukilprime/Savvy-Capstone/Dev/Pictures/DnD.jpg" alt="DnD"></img>
 <p>
-
+  The first roleplaying game to make mainstream popularity. Currently in its fifth edition, Dungeons and Dragons is one of the largest game systems today. The system is primarily a fantasy based and has a player create a character to inhabit the game world. A game master is required to run the game and create exciting situations for the players to navigate. You can play as your favorite fantasy race and swing a sword or sling spells to defeat the enemies before you!
+</p>
+</div>
+<div>
+<h4>
+Pathfinder
+</h4>
+<img src="https://raw.githubusercontent.com/Zukilprime/Savvy-Capstone/Dev/Pictures/Pathfinder.jpg" alt="Pathfinder"></img>
+<p>
+Pathfinder is the offshoot of an earlier edition (third edition to be precise) of Dungeons and Dragons. It was designed by Paizo to create their own system to give players dissatisfied with the upcoming fourth edition of Dungeons and Dragons and improved system that was similar to the outgoing edition of Dungeons and Dragons. It is now in its second edition and doing well. Pathfinder has made itself distinct from its originating game.
+</p>
+</div>
+<div>
+<h4>
+Warhammer Fantasy
+</h4>
+<img src="https://raw.githubusercontent.com/Zukilprime/Savvy-Capstone/Dev/Pictures/Warhammer_Fantasy_RedCraig.jpg" alt="Warhammer Fantasy"></img>
+<p>
+Warhammer fantasy is one of the older table top war games. You build an army of units to wage war on the opponent. Filled with colorful history and exciting races, no game plays out the same. Swarm the enemy with hordes of Orcs, or skillfully eradicate the opponent with the stoic Dwarves. There are many races to choose from and you are sure to find a favorite.
+</p>
+</div>
+<div>
+<h4>
+Warhammer 40K
+</h4>
+<img src="https://raw.githubusercontent.com/Zukilprime/Savvy-Capstone/Dev/Pictures/WH40K.jpg" alt="Warhammer 40K"></img>
+<p>
+Warhammer 40K is the futuristic table top war game in the same universe as Warhammer Fantasy. The races are now set far into the future where strange alien rivals and ancient empires flourish. Some of your favorites are still going strong, but they have been pushed back by new and exotic forces.
 </p>
 </div>
 </section>
